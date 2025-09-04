6fc1ea813efa07d2791cc60a457e52ab33d4696a
alexmerid <alexmerid@gmail.com>
2025-09-03 22:25:42 -0400

    Modificado nomarch

--- ejecucion.py
+++ ejecucion.py
@@ -13,7 +13,8 @@ DB_CONFIG = {
 conexion = pymysql.connect(**DB_CONFIG)
 cursor = conexion.cursor()
 
-nomarch = "/media/alexander/Unidad_E/Warnes/Ejecucion/2025-06/000.csv"
+# nomarch = "/media/alexander/Unidad_E/Warnes/Ejecucion/2025-06/000.csv"
+nomarch = input("Ingrese el nombre del archivo CSV (con ruta completa): ")
 
 with open(nomarch, "r", encoding="utf-8") as archivo:
     lector = csv.reader(archivo)
