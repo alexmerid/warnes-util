4a945adfb677824f84534b1c969aa491f1319466
Alexander Mérida <alexander@alex-pc.mydomain.lan>
2026-02-09 22:44:26 -0400

    verificación actualizada

--- verificacion.py
+++ verificacion.py
@@ -17,11 +17,19 @@ nomarch = input("Ingrese el nombre del archivo Excel (con ruta completa): ")
 
 wb = openpyxl.load_workbook(nomarch)
 hoja = wb.active
-
-print("Filas = ", hoja.max_row)
+# print("Filas = ", hoja.max_row)
 i = 1
 for fila in hoja.iter_rows(min_row=6, values_only=True):
     if fila[3] is not None:
-
-        print(f"{i:>4}. {fila[0]:>6} {fila[3]:>8}")
+        cursor.execute(
+            "SELECT id_poste FROM poste_luminaria WHERE codigo = %s", (fila[3],))
+        resultado = cursor.fetchone()
+        if resultado:
+            if resultado['id_poste'] == fila[0]:
+                print(f"{i:>4}. {fila[0]:>6} {fila[3]:>8} - OK")
+            else:
+                print(
+                    f"{i:>4}. {fila[0]:>6} {fila[3]:>8} -> {resultado['id_poste']:>6} - ERROR")
+        else:
+            print(f"{i:>4}. {fila[0]:>6} {fila[3]:>8} - NO EXISTE")
         i += 1
